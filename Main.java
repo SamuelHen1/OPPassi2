90e6ee211842a7f9ef82d5efdfc438780d93b030
SamuelHen1 <henriksen.samuel99@gmail.com>
2024-12-03 19:05:51 +0100

    test

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+        String menu = """
+                1. Register employee
+                2. manage salary
+                
+                """;
+        int option = 0;
+        switch(option){
+            case 1:
+                System.out.println("Enter employee file");
+            case 2:
+                System.out.println("Opt 2");
+        }
+    }
+
+}
