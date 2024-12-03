ac934c6a7fa551f47134f791beb16acb2de2b7c0
Sivert Bronebakk <sivertbronebakk@gmail.com>
2024-12-03 21:19:11 +0100

    Overloading for use for different positions

--- Company.java
+++ Company.java
@@ -12,11 +12,34 @@ public abstract class Company {
 
         return null;
     }
+
+
+    // For regular employees
     public void createEmployee(String id, String name, double grossSalary){
          Employee employee = new Employee(id, name, grossSalary);
          employees.add(employee);
-
     }
+
+
+    // For the managers
+    public void createEmployee(String id, String name, double grossSalary, String degree){
+        Employee employee = new Employee(id, name, grossSalary, degree);
+        employees.add(employee);
+    }
+
+
+    // For the students
+    public void createEmployee(String id, String name, int gpa){
+        Employee employee = new Employee(id, name,gpa);
+        employees.add(employee);
+    }
+    // For the directors
+    public void createEmployee(String id, String name, double grossSalary, String degree, String Department){
+        Employee employee = new Employee(id, name, grossSalary, degree, department);
+        employees.add(employee);
+    }
+
+
     public void removeEmployee(String id){
         Employee employee = retrieveEmployee(id);
         if (employee == null){
